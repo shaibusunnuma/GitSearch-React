22f51ac4e4e9910a95d7b00df4575f525eecd01c
shaibu <shaibusunnuma@gmail.com>
2021-04-12 20:50:48 +0000

    completed dashboard

---
+++ src/components/Charts/index.js
@@ -0,0 +1,7 @@
+import Column from "./Column";
+import Bar from "./Bar";
+import Pie from "./Pie";
+import Doughnut from "./Doughnut";
+import ExampleChart from "./ExampleChart";
+
+export { Pie, Column, Bar, Doughnut, ExampleChart };
---
+++ src/context/context.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import mockUser from './mockData.js/mockUser';
+import mockRepos from './mockData.js/mockRepos';
+import mockFollowers from './mockData.js/mockFollowers';
+import axios from 'axios';
+
+const rootUrl = 'https://api.github.com';
+
+
+
+
+const GithubContext = React.createContext();
+
+const GithubProvider = ({children}) =>{
+  const [githubUser, setGithubUser] = useState(mockUser)
+  const [repos, setRepos] = useState(mockRepos)
+  const [followers, setFollowers] = useState(mockFollowers)
+
+  const [requests, setRequests] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState({show: false, msg: ''})
+
+  const checkRequests = () =>{
+    axios(`${rootUrl}/rate_limit`).then(({data}) => {
+      let {rate: {remaining}} = data
+      setRequests(remaining)
+      if(remaining === 0){
+        toggleError(true, 'sorry you are out of requests!')
+      }
+    }).catch(error => console.log(error))
+  }
+
+  function toggleError(show=false, msg=''){
+    setError({show, msg})
+  }
+
+  useEffect(checkRequests,[])
+  
+  
+  return <GithubContext.Provider value={{githubUser, repos, followers, requests, error}}>{children}</GithubContext.Provider>
+}
+
+export  {GithubContext, GithubProvider};
